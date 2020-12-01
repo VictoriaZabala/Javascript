db2dba8c02116f20ad5aaf9bea23a51daf09fba6
Victoria Zabala <viqky_zabala@hotmail.com>
2020-12-01 13:59:42 -0300

    Clase 2

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-var numero1 = parseInt(prompt("Ingrese el nùmero a dividir:"));
-var resultado = numero1 / 2;
-alert("El resultado es: " + resultado)
+let dineroEnBilletera = 600
+let frutilla = 250
+let naranja = 100
+let manzana = 150
+
+if ( frutilla<=300 && naranja===100 && manzana<200 ) {
+console.log('Quiero un kilo de frutillas, un kilo de naranjas y un kilo de manzana, por favor') }
+
+else if (frutilla>300) {
+console.log('te agradezco, me llevo sólo la naranja y la manzana') }
+
+else if (naranja>200) {
+console.log('te agradezco, me llevo sólo la frutilla y la manzana')}
+
+else if (manzana>200) {
+console.log('te agradezco, me llevo sólo la frutilla y la naranja') } 
+
+else {
+console.log('Mejor voy a otra verduleria') }
+
+
+
+//CLASE 1: var numero1 = parseInt(prompt("Ingrese el nùmero a dividir:"));
+// var resultado = numero1 / 2;
+// alert("El resultado es: " + resultado)
+
