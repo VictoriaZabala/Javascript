f7d089d4317b3e408d548996eccec5ace6232c17
Victoria Zabala <viqky_zabala@hotmail.com>
2020-12-08 15:27:20 -0300

    clase 3

--- script.js
+++ script.js
@@ -1,22 +1,105 @@
-let dineroEnBilletera = 600
-let frutilla = 250
-let naranja = 100
-let manzana = 150
+let mayorDeEdad = Number(prompt("Ingrese su edad:"))
 
-if ( frutilla<=300 && naranja===100 && manzana<200 ) {
-console.log('Quiero un kilo de frutillas, un kilo de naranjas y un kilo de manzana, por favor') }
+while (mayorDeEdad <= 100) {
+	
 
-else if (frutilla>300) {
-console.log('te agradezco, me llevo sólo la naranja y la manzana') }
+	if (mayorDeEdad >= 18) {
+	console.log("Sos mayor de edad, podés ingresar.")
+	break;
+	}
 
-else if (naranja>200) {
-console.log('te agradezco, me llevo sólo la frutilla y la manzana')}
+	else {
+	console.log("Sos menor de edad, no podés ingresar.") 
+	break; }
 
-else if (manzana>200) {
-console.log('te agradezco, me llevo sólo la frutilla y la naranja') } 
+}
 
-else {
-console.log('Mejor voy a otra verduleria') }
+
+
+
+
+// for tiene que tener 3 condiciones, desde, hasta o mientras y la actualizacion del valor
+// for (let i = 0; i < 10; i++) {
+// 	console.log(i);
+// }
+
+// let i = 0
+// while (i < 10) {
+// 	console.log (i);
+// 	i++;
+// }
+
+// let i = 0;
+// let condicion = true; 
+// while (condicion) {
+// 	if (i == 7) {
+// 		condicion = false; 
+// 	}
+// 	console.log (i);
+// 	i++;
+// }
+
+// let j = 5;
+// let condicion = false;
+// do {
+// 	console.log (j);
+// 	j++;
+// } while (condicion);
+
+// el continue lo que hace es terminar con la iterecion y no ejecuta el codigo siguiente y vuelve
+// break termina el ciclo completo sale del for. muere ahì
+// diferencia entre el while y el do while es que el while podrìa no ejecutarse nunca, mientras que el do while mìnicmo una vez se va a ejecutar. se ejecuta antes de preguntar el while.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Clase 2 let dineroEnBilletera = 600
+// let frutilla = 250
+// let naranja = 100 
+// let manzana = 150
+
+// if ( frutilla<=300 && naranja===100 && manzana<200 ) {
+// console.log('Quiero un kilo de frutillas, un kilo de naranjas y un kilo de manzana, por favor') }
+
+// else if (frutilla>300) {
+// console.log('te agradezco, me llevo sólo la naranja y la manzana') }
+
+// else if (naranja>200) {
+// console.log('te agradezco, me llevo sólo la frutilla y la manzana')}
+
+// else if (manzana>200) {
+// console.log('te agradezco, me llevo sólo la frutilla y la naranja') } 
+
+// else {
+// console.log('Mejor voy a otra verduleria') }
 
 
 
