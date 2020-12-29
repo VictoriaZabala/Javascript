15b0245200723cf6a3b8755a97f11c54f1ee4e18
Victoria Zabala <viqky_zabala@hotmail.com>
2020-12-29 19:56:33 -0300

    Clase 8

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,31 @@
-let carrito = ["Plato Irregular" , "Bebedero para perros y gatos" , "Maceta colgante"];
+
+let carrito = document.createElement('h1');
+let insertar = document.querySelector('#insert')
 
 if (confirm("¿Quiere agregar estos productos al carrito?")) {
- console.log(`Los siguientes productos fueron agregados al carrito: `);
+carrito.textContent = "¡Productos añadidos al carrito correctamente!";
+carrito.style.textAlign = 'center';
+carrito.style.backgroundColor = '#ddbdff';
+
+insertar.appendChild(carrito);
 
 }
 
-carrito.forEach(item => console.log(item));
+// document.querySelector('.productos').remove();
 
-if (confirm(`¿Quiere eliminar ${carrito[1]} de su carrito?`)) {
-	carrito.splice(1, 1);
-}
-console.log("--------------------------------")
-console.log("Su carrito ha sido actualizado: ")
- carrito.forEach(item => console.log(item));
+
+// Clase 6 let carrito = ["Plato Irregular" , "Bebedero para perros y gatos" , "Maceta colgante"];
+
+// if (confirm("¿Quiere agregar estos productos al carrito?")) {
+//  console.log(`Los siguientes productos fueron agregados al carrito: `);
+
+// }
+
+// carrito.forEach(item => console.log(item));
+
+// if (confirm(`¿Quiere eliminar ${carrito[1]} de su carrito?`)) {
+// 	carrito.splice(1, 1);
+// }
+// console.log("--------------------------------")
+// console.log("Su carrito ha sido actualizado: ")
+//  carrito.forEach(item => console.log(item));
